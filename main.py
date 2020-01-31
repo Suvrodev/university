daf87134988003058200f720776d00fda908bd63
razerx100 <saikatmandal8@gmail.com>
2020-02-01 01:10:46 +0600

    0.1

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.8
+from string import String
+
+def main():
+    #print(String.Substring("tasty", 2, 3))
+    #print(String.Insert('ABCDEFG', 3, 'XYZ'))
+    #print(String.Delete('ABCDEFG', 3, 2))
+    print(String.Replace('XABYABZAB', 'AB', 'C'))
+
+main()
+
---
+++ string.py
@@ -0,0 +1,62 @@
+class String:
+    @classmethod
+    def Length(cls, string):
+        length = 0
+        try:
+            while True:
+                string[length]
+                length += 1
+        except IndexError:
+            return length
+
+    @classmethod
+    def Index(cls, text, pattern):
+        text_length = cls.Length(text)
+        pattern_length = cls.Length(pattern)
+        flag = True
+        for i in range(text_length - pattern_length + 1):
+            for j in range(pattern_length):
+                if text[i + j] != pattern[j]:
+                    flag = False
+                    break
+                else:
+                    flag = True
+            if flag:
+                return i
+        return 0
+
+    @classmethod
+    def Concatenate(cls, string1, string2):
+        new_str_size = cls.Length(string1) + cls.Length(string2)
+        new_str = [i for i in range(new_str_size)]
+        strlen1 = cls.Length(string1)
+        for i in range(cls.Length(string1)):
+            new_str[i] = string1[i]
+        for i in range(cls.Length(string2)):
+            new_str[strlen1 + i] = string2[i]
+        return "".join(new_str)
+
+    @classmethod
+    def Substring(cls, string, initial, length):
+        new_str = ""
+        for i in range(initial - 1, initial + length - 1):
+            new_str = cls.Concatenate(new_str, string[i])
+        return new_str
+
+    @classmethod
+    def Insert(cls, text, position, string):
+        return cls.Concatenate(cls.Concatenate(cls.Substring(text, 1, position - 1), string),
+                cls.Substring(text, position, cls.Length(text) - position + 1))
+
+    @classmethod
+    def Delete(cls, text, position, length):
+        return cls.Concatenate(cls.Substring(text, 1, position - 1),
+                cls.Substring(text, position + length, cls.Length(text) - position - length + 1))
+
+    @classmethod
+    def Replace(cls, text, pattern1, pattern2):
+        patern_position = cls.Index(text, pattern1)
+        while patern_position != 0:
+            text = cls.Replace(text, pattern1, pattern2)
+            patern_position = cls.Index(text, pattern1)
+        return text
