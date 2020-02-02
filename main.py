79677cec26f6bbf4f5fc947f082100c3d67a970e
razerx100 <saikatmandal*@gmail.com>
2020-02-02 13:05:53 +0000

    "0.12"

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3.8
 from string import String
+from array import Array
 
 def main():
     #print(String.Substring("tasty", 2, 3))
     #print(String.Insert('ABCDEFG', 3, 'XYZ'))
     #print(String.Delete('ABCDEFG', 3, 2))
-    print(String.Replace('XABYABZAB', 'AB', 'C'))
+    #print(String.Replace('XABYABZAB', 'AB', 'C'))
+    data = list(map(int, input().split()))
+    print(Array.bubble_sort(data))
 
 main()
 
