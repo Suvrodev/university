1a7bfa203f6884b7af2523c7636c1dab001eec42
razerx100 <saikatmandal8@gmail.com>
2020-03-02 00:15:55 +0600

    java update

--- Linked_List.java
+++ Linked_List.java
@@ -1,14 +1,11 @@
 class Node{
     int value;
-    int key;
     Node next;
-    public Node(){}
-    public Node(int key, int value){
+    public Node(int value){
         this.value = value;
-        this.key = key;
         this.next = null;
     }
-    public boolean if_next(){
+    public boolean has_next(){
         return this.next != null;
     }
     public Node get_next(){
@@ -25,37 +22,67 @@ class Node{
 public class Linked_List{
     Node node_head = null;
     public Linked_List(){}
-    public void add_last(int key, int value){
-        Node new_node = new Node(key, value);
+    public void add_last(int value){
+        Node new_node = new Node(value);
         if(node_head == null){
-            node_head = new Node(key, value);
+            node_head = new Node(value);
         }
         else{
-            Node var = node_head;
-            while(var.if_next()){
-                var = var.get_next();
-            }
+            Node var;
+            for(var = node_head; var.has_next(); var = var.get_next()){}
             var.set_next(new_node);
         }
     }
-    public void print_list(){
-        Node var = node_head;
+    public void traverse(){
         System.out.print("[");
-        while(var != null){
+        for(Node var = node_head; var != null; var = var.get_next()){
             System.out.print(" " + var.get_value());
-            var = var.get_next();
         }
         System.out.println(" ]");
     }
-    public void delete(int value){
-        Node current = node_head;
-        Node previous = null;
-        while(current != null){
-            if(current.get_value() == value){
-                previous.set_next(current.get_next());
+    public void remove(int value){
+        if(node_head.get_value() == value){
+            node_head = node_head.get_next();
+        }
+        else{
+            for(Node current = node_head, previous = null; current != null; previous = current, current = current.get_next()){
+                if(current.get_value() == value){
+                    previous.set_next(current.get_next());
+                }
             }
-            previous = current;
-            current = current.get_next();
+        }
+    }
+    public void insert(int data, int position){
+        int count = 0;
+        Node new_data = new Node(data);
+        if(position == 0){
+            new_data.set_next(node_head);
+            node_head = new_data;
+        }
+        else{
+            for(Node var = node_head, previous = null; var != null; previous = var, var = var.get_next(), count++){
+                if(position == count){
+                    new_data.set_next(var);
+                    previous.set_next(new_data);
+                    break;
+                }
+            }
+        }
+    }
+    public void insert_after(int data1, int data2){
+        Node new_data = new Node(data2);
+        if(node_head.get_value() == data1){
+            new_data.set_next(node_head.get_next());
+            node_head.set_next(new_data);
+        }
+        else{
+            for(Node var = node_head; var != null; var = var.get_next()){
+                if(var.get_value() == data1){
+                    new_data.set_next(var.get_next());
+                    var.set_next(new_data);
+                    break;
+                }
+            }
         }
     }
 }
--- maincls.java
+++ maincls.java
@@ -2,10 +2,10 @@ public class maincls{
     public static void main(String[] args){
         Linked_List list = new Linked_List();
         for(int i = 0; i < 5; i++){
-            list.add_last(i, 10 + i);
+            list.add_last(10 + i);
         }
-        list.print_list();
-        list.delete(12);
-        list.print_list();
+        list.traverse();
+        list.insert_after(12, 21);
+        list.traverse();
     }
 }
